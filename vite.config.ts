ca33833cc2c2ae99a8bc63fe09334d7c00c7200c
KobeVingelen <kobevd2012@hotmail.be>
2024-12-27 17:28:33 +0100

    nexxttt

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/', // Ensure this is set to root if not deploying in a subdirectory
+  resolve: {
+    alias: {
+      // Do NOT alias 'public' folder
+      // Avoid something like: '@public': '/src/public'
+    }
+  }
+})
